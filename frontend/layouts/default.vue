d290a5d4f96cbc37e622880a66597d3daf36d5aa
danielenriquez59 <deenriquez92@gmail.com>
2025-11-15 12:31:33 -0800

    edit to default bg

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-screen bg-white">
     <LandingHeader hide-hero />
     <div class="flex-1 container mx-auto z-0 px-4 py-6 lg:px-0">
       <slot />
