32c0b4b13965969b3fe5602c3f16a5f6e0a24749
danielenriquez59 <deenriquez92@gmail.com>
2025-11-15 13:19:54 -0800

    animate blobs on hero

--- frontend/layers/landing/components/landing/Header.vue
+++ frontend/layers/landing/components/landing/Header.vue
@@ -22,7 +22,7 @@ const navigation: NavigationItem[] = [
     }"
   >
     <div v-if="!hideBg" class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-      <svg class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
+      <svg class="blob-top relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
         <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
         <defs>
           <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
@@ -39,3 +39,24 @@ const navigation: NavigationItem[] = [
     <LandingHero v-if="!hideHero" />
   </div>
 </template>
+
+<style scoped>
+@keyframes blob-float-top {
+  0%, 100% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+  25% {
+    transform: translate(10px, -15px) rotate(5deg);
+  }
+  50% {
+    transform: translate(20px, 10px) rotate(10deg);
+  }
+  75% {
+    transform: translate(-15px, 5px) rotate(5deg);
+  }
+}
+
+.blob-top {
+  animation: blob-float-top 8s ease-in-out infinite;
+}
+</style>
--- frontend/layers/landing/components/landing/Hero.vue
+++ frontend/layers/landing/components/landing/Hero.vue
@@ -184,7 +184,7 @@ onClickOutside(searchContainerRef, () => {
         </div>
       </div>
       <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-        <svg class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
+        <svg class="blob-bottom relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" xmlns="http://www.w3.org/2000/svg">
           <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fill-opacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
           <defs>
             <linearGradient id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc" x1="1155.49" x2="-78.208" y1=".177" y2="474.645" gradientUnits="userSpaceOnUse">
@@ -197,3 +197,24 @@ onClickOutside(searchContainerRef, () => {
     </div>
   </div>
 </template>
+
+<style scoped>
+@keyframes blob-float-bottom {
+  0%, 100% {
+    transform: translate(0, 0) rotate(0deg);
+  }
+  25% {
+    transform: translate(-15px, 10px) rotate(-5deg);
+  }
+  50% {
+    transform: translate(-25px, -10px) rotate(-10deg);
+  }
+  75% {
+    transform: translate(10px, -5px) rotate(-5deg);
+  }
+}
+
+.blob-bottom {
+  animation: blob-float-bottom 8s ease-in-out infinite;
+}
+</style>
